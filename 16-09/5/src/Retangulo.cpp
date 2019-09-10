c58617029c80e910b1ddf8786436ebedea0325fd
Yuri Dimitre Dias de Faria <yuri.d.d.faria@gmail.com>
2019-09-10 09:37:06 -0300

    Retirado comentarios de desenvolvimento

--- 16-09/5/src/Retangulo.cpp
+++ 16-09/5/src/Retangulo.cpp
@@ -34,7 +34,6 @@ void Retangulo::decrementarLargura(){
     largura-=1;
 }
 double Retangulo::calcularPerimetro(){
-    //TODO:Descobrir como calcular o perimetro so com a largura e altura
     return 2*altura+2*largura;
 }
 double Retangulo::calcularArea(){
@@ -49,9 +48,3 @@ Retangulo::Retangulo(double altura,double largura){
         this->setAltura(altura);
         this->setLargura(largura);
 }
-/*Crie um método para Retangulo::incrementar a altura em 1.
-j. Crie um método para Retangulo::incrementar a largura em 1.
-k. Crie um método para Retangulo::decrementar a altura em 1.
-l. Crie um método para Retangulo::decrementar a largura em 1.
-m. Crie um método retornar o perímetro do retângulo.
-n. Crie um método retornar a área do retângulo*/
