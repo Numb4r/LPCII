718bb0134b0a138a9de949d5c7a262739c652012
Numb4r <yuri.d.d.faria@gmail.com>
2019-09-09 23:17:31 -0300

    Adicionado o arquivo CPP da classe Retangulo

---
+++ 16-09/5/src/Retangulo.cpp
@@ -0,0 +1,55 @@
+#include<iostream>
+#include"Retangulo.hpp"
+
+
+//Getters
+double Retangulo::getAltura(){
+    return altura;
+}
+double Retangulo::getLargura(){
+    return largura;
+}
+
+//Setters
+
+void Retangulo::setAltura(double altura){
+    this->altura = altura;
+}
+void Retangulo::setLargura(double largura){
+    this->largura = largura;
+}
+
+
+void Retangulo::incrementarAltura(){
+    altura+=1;
+}
+void Retangulo::incrementarLargura(){
+    largura+=1;
+}
+void Retangulo::decrementarAltura(){
+    altura-=1;
+}
+void Retangulo::decrementarLargura(){
+    largura-=1;
+}
+double Retangulo::calcularPerimetro(){
+    //TODO:Descobrir como calcular o perimetro so com a largura e altura
+}
+double Retangulo::calcularArea(){
+    return (altura*largura)/2;
+}
+//Construtor
+Retangulo::Retangulo(){
+        this->setAltura(0);
+        this->setLargura(0);
+}
+Retangulo::Retangulo(double altura,double largura){
+        this->setAltura(altura);
+        this->setLargura(largura);
+}
+/*Crie um método para Retangulo::incrementar a altura em 1.
+j. Crie um método para Retangulo::incrementar a largura em 1.
+k. Crie um método para Retangulo::decrementar a altura em 1.
+l. Crie um método para Retangulo::decrementar a largura em 1.
+m. Crie um método retornar o perímetro do retângulo.
+n. Crie um método retornar a área do retângulo*/
