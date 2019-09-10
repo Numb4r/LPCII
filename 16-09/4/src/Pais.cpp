de61e1830bedc00804168184571dd588de199d76
Yuri Dimitre Dias de Faria <yuri.d.d.faria@gmail.com>
2019-09-10 10:50:33 -0300

    Adicionado classe Pais

---
+++ 16-09/4/src/Pais.cpp
@@ -0,0 +1,31 @@
+#include "Pais.hpp"
+#include <iostream>
+
+//Getters 
+string Pais::getNome(){
+    return nome;
+}
+int Pais::getPopulacao(){
+    return populacao;
+}
+double Pais::getArea(){
+    return area;
+}
+
+//Setters
+void Pais::setNome(string nome){
+    this->nome = nome;
+}
+void Pais::setPopulacao(unsigned int populacao){
+    this->populacao = populacao;
+}
+void Pais::setArea(double area){
+    this->area = area < 0 ? -1*area : area;
+}
+
+//Construtor
+Pais::Pais(string nome,unsigned int populacao,double area){
+    this->nome = nome;
+    this->populacao = populacao;
+    this->setArea(area);
+}
---
+++ 16-09/4/src/Pais.hpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+#ifndef __PAIS_HPP
+#define __PAIS_HPP
+class Pais
+{
+    private:
+        string nome;
+        int populacao;
+        double area;
+
+    public:
+        //Getters
+        string getNome();
+        int getPopulacao();
+        double getArea();
+        //Setters
+        void setNome(string nome);
+        void setPopulacao(unsigned int populacao);
+        void setArea(double area);
+        //Construtor
+        Pais();
+        Pais(string nome,unsigned int populaco,double area);
+
+};
+
+#endif
