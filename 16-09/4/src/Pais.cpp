5e0067639a2a5020a3f9d62d212efcb249cc771b
Yuri Dimitre Dias de Faria <yuri.d.d.faria@gmail.com>
2019-09-12 11:40:25 -0300

    Declarado construtor padrao

--- 16-09/4/src/Pais.cpp
+++ 16-09/4/src/Pais.cpp
@@ -24,6 +24,7 @@ void Pais::setArea(double area){
 }
 
 //Construtor
+Pais::Pais(){}
 Pais::Pais(string nome,unsigned int populacao,double area){
     this->nome = nome;
     this->populacao = populacao;
