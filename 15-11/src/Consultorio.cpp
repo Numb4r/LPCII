2ca2bc08d6b457c7e369fbfaea85102f83f37d35
Numb4r <yuri.d.d.faria@gmail.com>
2019-11-15 17:09:20 -0300

    Terminada classe consultorio

--- 15-11/src/Consultorio.cpp
+++ 15-11/src/Consultorio.cpp
@@ -7,79 +7,147 @@ void Consultorio::cadastrarMedico(Medico medico){
     vetMedicos.push_back(medico);
 }
 bool Consultorio::removerPaciente(int CPF){
-    unsigned long int i = 0;
-    for (i = 0; i < vetPacientes.size(); i++)
+    
+    for (unsigned long int i = 0; i < vetPacientes.size(); i++)
     {
-        if(vetPacientes.at(i).getCPF() == CPF)
-            break;
-        else
-            return false;//corrigir o retorno no primeiro caso
+        if(vetPacientes.at(i).getCPF() == CPF){
+            vetPacientes.erase(vetPacientes.begin()+i) ;
+            while (this->removerConsultaByCPF(CPF)){}
+            return true;
+        }
     }
-    vetPacientes.erase(vetPacientes.begin()+i);
-    //Remover consultas relacionadas
-    return true;
-
+    return false;
 }
 bool Consultorio::removerMedico(int CRM){
     unsigned long int i = 0;
     for(i = 0; i< vetMedicos.size();i++){
-        if(vetMedicos.at(i).getCRM() == CRM)
-            break;
-        else
-            return false;//corrigir o retorno no primeiro caso
-        
+        if(vetMedicos.at(i).getCRM() == CRM){
+            vetMedicos.erase(vetMedicos.begin()+1);
+            while (this->removerConsultaByCRM(CRM)){}
+            return true;
+        }
     }
-    vetMedicos.erase(vetMedicos.begin()+i);
-    //Remover consultas relacionadas
-    return true;
+    return false;
 }
 void Consultorio::cadastrarConsulta(Consulta consulta){
     vetConsulta.push_back(consulta);
 }
-bool Consultorio::removerConsultaByCPF(int cpf,std::string data = "0"){
-    unsigned long int i = 0;
-    for (i = 0; i < vetConsulta.size(); i++)
+bool Consultorio::removerConsultaByCPF(int cpf,std::string data ){
+    
+    for (unsigned long int i = 0; i < vetConsulta.size(); i++)
     {
         if(vetConsulta.at(i).getPaciente().getCPF() == cpf){
-            
-        }else{
-            return false;
+            if (vetConsulta.at(i).getData() == data)
+            {
+                vetConsulta.erase(vetConsulta.begin()+1);    
+                return true;
+            }else{
+                vetConsulta.erase(vetConsulta.begin()+1);
+                return true;
+            }
         }
     }
-    return true;
+    return false;
 }
-bool Consultorio::removerConsultaByCRM(int crm,std::string data = "0"){
+bool Consultorio::removerConsultaByCRM(int crm,std::string data ){
+    for (unsigned long int i = 0; i < vetConsulta.size(); i++)
+    {
+        if(vetConsulta.at(i).getMedico().getCRM() == crm){
+            if (vetConsulta.at(i).getData() == data)
+            {
+                vetConsulta.erase(vetConsulta.begin()+1);    
+                return true;
+            }else{
+                vetConsulta.erase(vetConsulta.begin()+1);
+                return true;
+            }
+        }
+    }
+    return false;
 
 }
 void Consultorio::imprimirListaPacientes(){
-
+    for (long unsigned int i = 0; i < vetPacientes.size(); i++)
+    {
+        cout<<vetPacientes.at(i).getNome()<<endl;
+        
+    }
 }
 void Consultorio::imprimirListaMedicos(){
-
+    for (long unsigned int i = 0; i < vetMedicos.size(); i++)
+    {
+        cout<<vetMedicos.at(i).getNome()<<endl;
+    }
 }
 void Consultorio::imprimirListaConsultas(){
 
-}
-void Consultorio::imprimirListaMedico(int crm,std::string data = "0"){
+    for (long unsigned int i = 0; i < vetConsulta.size(); i++)
+    {
+        vetConsulta.at(i).imprimir();
+        cout<<"\n------------------------------------------"<<endl;
+    }
+    
 
 }
-
+void Consultorio::imprimirListaConsultasMedico(int crm,std::string data){
+    for (long unsigned int i = 0; i < vetConsulta.size(); i++)
+    {
+        if (vetConsulta.at(i).getMedico().getCRM() == crm)
+        {    
+            if (vetConsulta.at(i).getData() == data)
+            {
+                vetConsulta.at(i).imprimir();
+            }else{
+               vetConsulta.at(i).imprimir();
+            }
+            cout<<"\n------------------------------------------"<<endl;
+        }
+        
+    }
+    
+}
+Paciente Consultorio::getPacienteByCPF(int cpf){
+    for (long unsigned int i = 0; i < vetPacientes.size(); i++)
+        if (vetPacientes.at(i).getCPF() == cpf)
+            return vetPacientes.at(i);
+    return Paciente();
+}
+Medico Consultorio::getMedicoByCRM(int crm){
+    for (long unsigned int i = 0; i < vetMedicos.size(); i++)
+        if (vetMedicos.at(i).getCPF() == crm)        
+            return vetMedicos.at(i);     
+    return Medico();
+}
+bool Consultorio::removerConsulta(int crm,int cpf){
+    for (long unsigned int i = 0; i < vetConsulta.size(); i++){
+        if(vetConsulta.at(i).getMedico().getCRM() == crm && vetConsulta.at(i).getPaciente().getCPF() == cpf){
+            vetConsulta.erase(vetConsulta.begin()+i);
+            return true;
+        }
+    }
+    return false;
+}
 //Setters
-void Consultorio::setNome(std::string nome){}
-void Consultorio::setEndereco(std::string endereco){}
-void Consultorio::setTelefone(std::string telefone){}
+void Consultorio::setNome(std::string nome){
+    this->nome = nome;
+}
+void Consultorio::setEndereco(std::string endereco){
+    this->endereco = endereco;
+}
+void Consultorio::setTelefone(std::string telefone){
+    this->telefone = telefone;
+}
 //Getters
 std::string Consultorio::getNome(){
+    return nome;
 
 }
 std::string Consultorio::getEndereco(){
-
+    return endereco;
 }
 std::string Consultorio::getTelefone(){
-
+    return telefone;
 }
-
-
 Consultorio::Consultorio(std::string nome,std::string endereco,std::string telefone){
     this->nome = nome;
     this->endereco = endereco;
--- 15-11/src/Consultorio.hpp
+++ 15-11/src/Consultorio.hpp
@@ -23,11 +23,13 @@ public:
     void cadastrarConsulta(Consulta consulta);
     bool removerConsultaByCPF(int cpf,std::string data = "0");
     bool removerConsultaByCRM(int crm,std::string data = "0");
+    bool removerConsulta(int crm,int cpf);
     void imprimirListaPacientes();
     void imprimirListaMedicos();
     void imprimirListaConsultas();
-    void imprimirListaMedico(int crm,std::string data = "0");
-
+    void imprimirListaConsultasMedico(int crm,std::string data = "0");
+    Paciente getPacienteByCPF(int cpf);
+    Medico getMedicoByCRM(int crm);
     //Setters
     void setNome(std::string nome);
     void setEndereco(std::string endereco);
